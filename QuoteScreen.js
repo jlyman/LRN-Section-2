00652d6884216f3ce8b2ef87c9388de8a8c95543
Joshua Lyman <jlyman.work@gmail.com>
2016-07-10 00:08:18 -0500

    Added animation to the changing of our quotes. Needed to add keys to Quote component and add a lifecycle method to support animation.

--- QuoteScreen.js
+++ QuoteScreen.js
@@ -3,18 +3,45 @@ import {
 	StyleSheet,
 	View,
 	Image,
+	LayoutAnimation,
 } from 'react-native'
 import Quote from './Quote'
 import NextQuoteButton from './NextQuoteButton'
 
 const bgImage = require('./assets/bg.png')
+const tranquil = {
+	duration: 500,
+	create: {
+		duration: 1000,
+		delay: 300,
+		type: LayoutAnimation.Types.easeIn,
+		property: LayoutAnimation.Properties.opacity,
+	},
+	update: {
+		type: LayoutAnimation.Types.easeInEaseOut,
+		property: LayoutAnimation.Properties.opacity,
+	},
+	delete: {
+		duration: 200,
+		type: LayoutAnimation.Types.easeOut,
+		property: LayoutAnimation.Properties.opacity,
+	},
+}
 
 class QuoteScreen extends Component {
+	componentWillUpdate() {
+		LayoutAnimation.configureNext(tranquil)
+	}
+
 	render() {
 		return (
 			<Image source={bgImage} style={styles.backgroundContainer}>
 				<View style={styles.container}>
-					<Quote quoteText={this.props.text} quoteSource={this.props.source} />
+					<Quote
+						key={this.props.qId}
+						quoteText={this.props.text}
+						quoteSource={this.props.source}
+					/>
 					<NextQuoteButton onPress={this.props.onNextQuotePress} />
 				</View>
 			</Image>
@@ -26,6 +53,7 @@ QuoteScreen.propTypes = {
 	text: PropTypes.string.isRequired,
 	source: PropTypes.string.isRequired,
 	onNextQuotePress: PropTypes.func.isRequired,
+	qId: PropTypes.number.isRequired,
 }
 
 const styles = StyleSheet.create({
