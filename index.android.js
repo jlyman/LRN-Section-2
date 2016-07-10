54565f5d774c36b4707a2d7fccf883e364a79728
Joshua Lyman <jlyman.work@gmail.com>
2016-07-09 22:44:32 -0500

    Updated first text component and changed the styling of the main screen.

--- index.android.js
+++ index.android.js
@@ -6,47 +6,48 @@
 
 import React, { Component } from 'react'
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
+	AppRegistry,
+	StyleSheet,
+	Text,
 	View,
 } from 'react-native'
 
 class RelaxationStation extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					Welcome to My App!
+				</Text>
+				<Text style={styles.instructions}>
+					To get started, edit index.android.js
+				</Text>
+				<Text style={styles.instructions}>
+					Shake or press menu button for dev menu
+				</Text>
+			</View>
 		)
-  }
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#161737',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+		color: '#ffffff',
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#dbdbdb',
+		marginBottom: 5,
+	},
 })
 
 AppRegistry.registerComponent('RelaxationStation', () => RelaxationStation)
--- index.ios.js
+++ index.ios.js
@@ -6,48 +6,49 @@
 
 import React, { Component } from 'react'
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
+	AppRegistry,
+	StyleSheet,
+	Text,
 	View,
 } from 'react-native'
 
 class RelaxationStation extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					Welcome to My App!
+				</Text>
+				<Text style={styles.instructions}>
+					To get started, edit index.ios.js
+				</Text>
+				<Text style={styles.instructions}>
+					Press Cmd+R to reload,{'\n'}
+					Cmd+D or shake for dev menu
+				</Text>
+			</View>
 		)
-  }
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#161737',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+		color: '#ffffff',
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#dbdbdb',
+		marginBottom: 5,
+	},
 })
 
 AppRegistry.registerComponent('RelaxationStation', () => RelaxationStation)
